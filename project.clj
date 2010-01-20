b9b42183b8f16388f2d2700636f88d5e7bf66961
Sergey Pariev <spariev@gmail.com>
2010-01-20 16:24:31 +0300

    lein project file

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject spariev/chrono "1.0.0-SNAPSHOT" 
+  :description "Fork of incanter.chrono date library" 
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"] 
+		 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
+		 [joda-time/joda-time "1.6"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+  )
